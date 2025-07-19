6920048167e770c9f1a105dbc37e96f336602c3f
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-07-19 15:05:56 +0200

    UPD - Snack 4 done

---
+++ snacks/snack-4.js
@@ -0,0 +1,12 @@
+const books = require(`../books`);
+
+const ages = books.map(book => book.author.age);
+console.log(`le età degli auronti sono:\n${ages}`);
+
+const agesSum = ages.reduce((acc, age) => {
+    return acc += age;
+}, 0)
+console.log(`La soma delle età è: ${agesSum}`);
+
+const averageAge = agesSum / ages.length;
+console.log(`L'età media è di: ${averageAge}`);
